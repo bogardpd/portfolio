d871e5a850795e487dfffb4ad7bd6ab6aa0bb00f
Paul Bogard <paulbogard@gmail.com>
2016-04-18 07:47:18 -0400

    Add routes and controller actions and update links for all project pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,13 +6,34 @@ class StaticPagesController < ApplicationController
   def about
   end
   
+  def cad_models
+  end
+  
   def computers
+  end
+  
+  def earthbound_database
   end
   
   def flight_historian
   end
   
+  def gps_logging
+  end
+  
+  def hotel_internet_quality
+  end
+  
   def resume
+  end
+  
+  def terminal_silhouettes
+  end
+  
+  def turn_signal_counter
+  end
+  
+  def visor_cam
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   # Projects
+  get 'cad-models' => 'static_pages#cad_models'
   get 'earthbound-database' => 'static_pages#earthbound_database'
   get 'flight-historian' => 'static_pages#flight_historian'
+  get 'gps-logging' => 'static_pages#gps_logging'
+  get 'hotel-internet-quality' => 'static_pages#hotel_internet_quality'
+  get 'terminal-silhouettes' => 'static_pages#terminal_silhouettes'
+  get 'turn-signal-counter' => 'static_pages#turn_signal_counter'
+  get 'visor-cam' => 'static_pages#visor_cam'
   
   # About Pages
   get 'about' => 'static_pages#about'
