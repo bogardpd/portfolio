2ce50da3ac6541a8a150bda160b93aac60fafc14
Paul Bogard <paulbogard@gmail.com>
2016-04-20 22:00:12 -0400

    Create iOS to Google Earth tutorial page

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,9 +1,13 @@
 module StaticPagesHelper
   
   # Returns a responsive image.
-  def project_image(image_path, alt_text='')
+  def project_image(image_path, alt_text='', ios_screenshot=false)
     html = "<div class=\"image\">"
-    html += image_tag(image_path, class: 'img-responsive project', alt: alt_text)
+    if ios_screenshot
+      html += image_tag(image_path, class: 'img-responsive project ios-screenshot', alt: alt_text)
+    else
+      html += image_tag(image_path, class: 'img-responsive project', alt: alt_text)
+    end
     html += "</div>"
     html.html_safe
   end
