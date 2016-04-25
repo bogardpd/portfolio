bbb4775f4a57922e731096cd6239f5c5dfbef2e6
Paul Bogard <paulbogard@gmail.com>
2016-04-24 22:31:27 -0400

    Add meta descriptions to pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,14 @@ module ApplicationHelper
   def format_date(input_date)
     input_date.strftime("%e %b %Y").strip
   end
+  
+  # Returns the meta description on a per-page basis.
+  def meta_description(page_meta_description = '')
+    if page_meta_description.empty?
+      ""
+    else
+      "<meta name=\"description\" content=\"#{page_meta_description}\" />".html_safe
+    end
+  end
+  
 end
