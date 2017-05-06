00fac85e38f159020fbcf4b5fa96cbe12de1b65f
Paul Bogard <paulbogard@gmail.com>
2017-05-05 22:35:12 -0400

    Create basic updateChart functino for Time Zone Chart

--- app/assets/javascripts/time_zones.js
+++ app/assets/javascripts/time_zones.js
@@ -1,5 +1,22 @@
+var rowCount;
+
 $(function() {
-  console.log('time_zones.js loaded')
-  $('.datetimepicker').datetimepicker({format: 'YYYY-MM-DD HH:mm'});
+  $(".datetimepicker").datetimepicker({format: "YYYY-MM-DD HH:mm"});
+  rowCount = $("tr.location-row").length;
+
+  $("input").on("blur", updateChart);
+  $("select").on("blur", updateChart);
 });
 
+function updateChart() {
+  var timeZoneLocations = [];
+  for(i = 0; i < rowCount; i++) {
+    timeZoneLocations[i] = {
+      start:    $("#start_"+i).val(),
+      location: $("#location_"+i).val(),
+      offset:   parseFloat($("#offset_"+i).val()),
+      end:      $("#end_"+i).val()
+    };
+  }
+  $("#chart").text(JSON.stringify(timeZoneLocations));
+}
