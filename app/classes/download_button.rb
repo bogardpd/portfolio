b6bba65f343923f1470c335f4cfa798a0c3862a8
Paul Bogard <paulbogard@gmail.com>
2017-09-10 18:53:54 -0400

    Fix AWS socket error when AWS not available on DownloadButton class

--- app/classes/download_button.rb
+++ app/classes/download_button.rb
@@ -6,7 +6,6 @@ class DownloadButton < LinkButton
     s3 = Aws::S3::Resource.new(client: client)
     obj = s3.bucket("pbogardcom-files").object(path)
 
-    puts path
     if obj.exists?
       @path = obj.public_url
       @size = obj.content_length
@@ -14,17 +13,26 @@ class DownloadButton < LinkButton
       @icon = "filetypes/unknown" unless Rails.application.assets.find_asset("icons/#{@icon}.png")
       @text = "<strong>#{URI.decode(File.basename(@path))}</strong> (#{number_to_human_size(@size)})"
     else
-      @path = ""
-      @icon = "filetypes/unknown"
-      @text = "Could not find file"
-      @size = 0
+      set_file_not_found_values
     end
+    
+  rescue Aws::Errors::NoSuchEndpointError
+    set_file_not_found_values
      
   end
   
   def plain_link
     return nil if @path.blank?
     return %Q(#{link_to(URI.decode(File.basename(@path)), @path)} (#{number_to_human_size(@size)})).html_safe
+  end
+  
+  private
+  
+  def set_file_not_found_values
+    @path = ""
+    @icon = "filetypes/unknown"
+    @text = "Could not find file"
+    @size = 0
   end
   
 end
