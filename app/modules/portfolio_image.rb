671a54c60caaf133f2882de0cdb8a8ddd3612a8d
Paul Bogard <paulbogard@gmail.com>
2018-06-19 22:11:57 -0400

    Update asset check

--- app/modules/portfolio_image.rb
+++ app/modules/portfolio_image.rb
@@ -1,3 +1,11 @@
 module PortfolioImage
   ROOT_PATH = "https://s3.us-east-2.amazonaws.com/pbogardcom-images/"
+
+  def asset_exist?(path)
+    if Rails.configuration.assets.compile
+      Rails.application.precompiled_assets.include? path
+    else
+      Rails.application.assets_manifest.assets[path].present?
+    end
+  end
 end
