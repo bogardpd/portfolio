105dfb899b7d052d4271a1ba573d30101d13e75e
Paul Bogard <paulbogard@gmail.com>
2019-02-19 20:52:36 -0500

    Remove html_safe from Tile class

--- app/classes/tile.rb
+++ app/classes/tile.rb
@@ -2,33 +2,27 @@
 class Tile
   include ActionView::Helpers
   
-  def initialize(caption: nil, img: nil, path: nil, title: nil, theme: nil, columns: nil)
+  def initialize(caption: nil, img: nil, path: nil, title: nil, date_string: nil, theme: nil, columns: nil)
     @path = path
     @title = title
     @img = img
     @caption = caption
+    @date_string = date_string
     @theme = theme
     @columns = columns || %w(col-xl-4 col-lg-6)
   end
   
   def show
-    html = String.new
-    tile = String.new
-    
+    tile = ActiveSupport::SafeBuffer.new
     
     tile << image_tag(image_source, class: "img-fluid") if image_source
-    tile << "<h3>#{title}</h3>" if title
-    tile << "<p>#{caption}</p>" if caption
+    tile << content_tag(:h3, title) if title
+    tile << content_tag(:p, caption) if caption
+    tile << content_tag(:p, date_string) if date_string
+
+    tile = link_to(tile, path, class: %w(btn btn-link btn-block)) if path
     
-    html << %Q(<div class="#{tile_classes}">)
-    if path
-      html << link_to(tile.html_safe, @path, class: "btn btn-link btn-block")
-    else
-      html << tile.html_safe
-    end
-    html << "</div>"
-    
-    return html.html_safe
+    return content_tag(:div, tile, class: tile_classes)
   end
   
   private
@@ -37,28 +31,32 @@ class Tile
     return @caption
   end
   
+  def classes_custom
+    return nil
+  end
+  
+  def date_string
+    return @date_string
+  end
+
+  def image_source
+    return @img
+  end
+  
+  def path
+    return @path
+  end
+  
   def tile_classes
     classes = ["tile"]
     classes << @columns
     classes << classes_custom
     classes << @theme if @theme
-    return classes.compact.join(" ")
-  end
-  
-  def classes_custom
-    return nil
-  end
-  
-  def image_source
-    return @img
+    return classes.compact
   end
   
   def title
     return @title
-  end
-  
-  def path
-    return @path
   end
   
 end
