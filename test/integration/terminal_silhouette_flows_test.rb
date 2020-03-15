3600cf9ca20f1d4d08765d5db7930bb63804c1f3
Paul Bogard <paulbogard@gmail.com>
2020-03-15 12:16:17 -0400

    Write integration tests for terminal silhouettes

--- test/integration/terminal_silhouette_flows_test.rb
+++ test/integration/terminal_silhouette_flows_test.rb
@@ -1,9 +1,23 @@
 require 'test_helper'
 
 class TerminalSilhouetteFlowsTest < ActionDispatch::IntegrationTest
-  # projects/terminal-silhouettes
+  
+  test "should get terminal silhouettes" do
+    get(terminal_silhouettes_path)
+    assert_response(:success)
+  end
   
   # REDIRECTS
 
-  # various terminal silhouette redirects
+  test "should redirect alternate terminal silhouette paths" do
+    alternate_paths = %w(terminal-silhouettes terminal_silhouettes terminalsilhouettes terminals)
+
+    alternate_paths.each do |alt_path|
+      get("/#{alt_path}")
+      assert_response(301)
+      assert_redirected_to(terminal_silhouettes_path)
+    end
+
+  end
+
 end
