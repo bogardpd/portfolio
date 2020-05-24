bb6a7e1aa7d73d17bece1359a987e383016fe511
Paul Bogard <paulbogard@gmail.com>
2020-05-24 12:20:10 -0400

    Add associations and validations to Part and PartCategory models

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,2 +1,6 @@
 class Part < ApplicationRecord
+  has_and_belongs_to_many :part_categories
+  validates :model, presence: true
+  validates :purchase_date, presence: true
+
 end
--- app/models/part_category.rb
+++ app/models/part_category.rb
@@ -1,2 +1,5 @@
 class PartCategory < ApplicationRecord
+  has_and_belongs_to_many :parts
+  validates :name, presence: true
+
 end
